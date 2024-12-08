309d826dbd4080bc9a5e2d2e768eef3ac758a305
Gaini <gainizhumabek@gmail.com>
2024-12-09 03:07:57 +0500

    Fixed

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,4 @@ export default function Home() {
       <p>Use the navigation menu to log in or register and explore recipes.</p>
     </div>
   );
-};
-
-Home.getInitialProps = async () => {
-  const data = await fetchSomeData();
-  return { data };
-};
-
-
+}
