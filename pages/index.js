e44dfc54913a609414f6cc681afff0b491ebfc1e
Gaini <gainizhumabek@gmail.com>
2024-12-10 00:26:21 +0500

    Added

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,13 @@ import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to Recipe Sharing App!</h1>
-      <p>Use the navigation menu to log in or register and explore recipes.</p>
-    </div>
+    <Layout>
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mt-8">Welcome to Recipe Sharing App!</h1>
+        <p className="mt-4 text-lg">
+          Use the navigation menu to log in, register, and explore recipes!
+        </p>
+      </div>
+    </Layout>
   );
 }
